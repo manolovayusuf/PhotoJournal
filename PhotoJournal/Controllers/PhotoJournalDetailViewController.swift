8a5d992bd245a560b2fe3c601ef0673d113be16f
Manny Yusuf <manolovayusuf@pursuit.org>
2019-01-15 10:55:52 -0500

    First commit

---
+++ PhotoJournal/Controllers/PhotoJournalDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoJournalDetailViewController.swift
+//  PhotoJournal
+//
+//  Created by Manny Yusuf on 1/14/19.
+//  Copyright © 2019 Manny Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ PhotoJournal/Controllers/PhotoJournalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Manny Yusuf on 1/14/19.
+//  Copyright © 2019 Manny Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
